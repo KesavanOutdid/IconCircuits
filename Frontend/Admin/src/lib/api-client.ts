2dd5480669e5221233353812a456a9d409df669c
KesavanOutdid <kesavan@outdidtech.com>
2025-12-23 16:51:16 +0530

    updated

--- Frontend/Admin/src/lib/api-client.ts
+++ Frontend/Admin/src/lib/api-client.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.0.6:9000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.13:9000";
 
 interface ApiResponse<T> {
   success: boolean;
